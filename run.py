bc7cf75dd6b71d7cd292e0df02fb9a1b942546d7
Derrick Gooden <dsgooden@gmail.com>
2018-09-27 09:59:49 -0400

    Modified run.py

--- run.py
+++ run.py
@@ -1,13 +1,18 @@
-from dcm_api import run_and_download_report
+import click
 
-if __name__ == '__main__':
-    profile_id = input("Enter your DCM profile ID: ")
-    report_id = input("Enter report ID: ")
-    path = input("Enter destination or press Enter for default location: ")
+from api import run_and_download_report
 
+
+@click.command()
+@click.option('--profile_id', prompt="Enter your DCM profile ID", help="Your DCM profile ID")
+@click.option('--report_id', prompt="Enter report ID", help="Your DCM report ID")
+@click.option('--path', default=None, help="Report save location")
+def main(profile_id, report_id, path):
     if not path:
         path = None
 
     run_and_download_report(profile_id=profile_id, report_id=report_id, path=path)
-    print()
-    input("Press Enter to exit")
+
+
+if __name__ == '__main__':
+    main()
